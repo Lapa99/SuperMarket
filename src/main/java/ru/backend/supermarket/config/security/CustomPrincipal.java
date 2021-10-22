5a6f03e988ef2071ad5332d5e6a1143ede72390a
Lapa99 <ph.lapa@yandex.ru>
2021-10-22 12:19:11 +0300

    Changelogs

---
+++ src/main/java/ru/backend/supermarket/config/security/CustomPrincipal.java
@@ -0,0 +1,53 @@
+package ru.backend.supermarket.config.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import ru.backend.supermarket.model.User;
+
+import java.util.*;
+
+public class CustomPrincipal implements UserDetails {
+    private final User user;
+    private final List<GrantedAuthority> authorities;
+
+    public CustomPrincipal(User user) {
+        this.user = user;
+        this.authorities = Collections.singletonList(new SimpleGrantedAuthority(user.getRole()));
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
